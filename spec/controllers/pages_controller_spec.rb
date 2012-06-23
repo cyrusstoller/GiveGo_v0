1dbbf7c38eaa9555925b4d725a20c6166574df82
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-06-23 15:07:23 -0700

    Initial commit

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+
+  describe "GET 'welcome'" do
+    it "returns http success" do
+      stub_request(:get, "https://api.singly.com/profiles?access_token=").
+               to_return(:status => 200, :body => "", :headers => {})
+               
+      get 'welcome'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+end
